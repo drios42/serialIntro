52b60c3892529d6491e280e854c3a5acb2a40cd7
drios42 <drios42@gmail.com>
2017-03-06 11:56:55 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+var serial; // variable to hold an instance of the serialport library
+var portName = '/dev/cu.usbmodem1411'; // fill in your serial port name here
+
+function setup() {
+  
+  serial = new p5.SerialPort(); // make a new instance of the serialport library
+  serial.on('list', printList); // set a callback function for the serialport list event
+  serial.on('connected', serverConnected); // callback for connecting to the server
+  serial.on('open', portOpen); // callback for the port opening
+  serial.on('data', serialEvent); // callback for when new data arrives
+  serial.on('error', serialError); // callback for errors
+  serial.on('close', portClose); // callback for the port closing
+
+  //serial.list(); // list the serial ports
+  serial.open(portName); // open a serial port
+ 
+}
+
+function draw() {
+ 
+}
+
+
+function serialEvent() {
+ 
+}
+
+function serialError(err) {
+  console.log('Something went wrong with the serial port. ' + err);
+}
+
+function portClose() {
+  console.log('The serial port closed.');
+}
+
+function printList(portList) {
+  for (var i = 0; i < portList.length; i++) {
+    console.log(i + " " + portList[i]);
+  }
+}
+
+
+function serverConnected() {
+  console.log('connected to server.');
+}
+
+function portOpen() {
+  console.log('the serial port opened.')
+}
